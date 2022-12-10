3529919029e7be3d30c37560e9ce46490adb36f2
Katia Menshikoff <kmenshikoff@itba.edu.ar>
2022-12-09 22:52:56 -0300

    Created an array, opened sensors.csv

--- tad.c
+++ tad.c
@@ -1,7 +1,39 @@
 #include <stdlib.h>
+#include <stdio.h>
+#define MAX 84
 
-typedef struct listCDT {
-    //empty
-}listCDT;
+typedef struct sensor {
+    char * name; 
+    char flag;
+    size_t len;
+}TSensor;
+
+TSensor * makeVec(void){
+    TSensor * ans;
+    FILE * fSensor;
+    fSensor = fopen("sensor.csv", "rt");
+    char line[100];
+    if (fSensor == NULL){
+        perror("Unable to open the file.");
+        exit(1);
+    }
+    while (!feof(fSensor)){
+        for (int i=0; fgets(line, 100, fSensor); i++){
+            char * value = strtok(line, ", ");
+            while (value != NULL){
+                char pos = value;
+                value = strtok(NULL, ", ");
+                ans[pos - 1].len = strlen(value);
+                ans[pos - 1].name = malloc(ans[pos - 1].len + 1); //vemos 
+                ans[pos - 1].name = strcpy(ans[pos - 1].name, value); //vemos 
+                value = strtok(NULL, ", ");
+                ans[pos - 1].flag = value;
+                value = strtok(NULL, ", ");
+            }
+        }
+    }
+    fclose(fSensor);
+    return ans;
+}
 
 
